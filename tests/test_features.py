212898e9c2b39241e36320980406f350d67c7cb4
Tomperez98 <tomasperezalvarez@gmail.com>
2024-10-29 17:34:14 -0500

    Test factorial multi and single node

--- tests/test_features.py
+++ tests/test_features.py
@@ -72,13 +72,45 @@ def test_factorial_same_node() -> None:
             return 1
         return n * (
             yield ctx.rfc(factorial, n - 1).with_options(
-                f"factorial-{n-1}", target=node_group
+                f"factorial-same-node-{n-1}", target=node_group
             )
         )
 
     store = RemoteServer(url=os.environ["RESONATE_STORE_URL"])
     s = Scheduler(store, logic_group=node_group)
     s.register(factorial)
-    n = 2
-    p: Promise[int] = s.run(f"factorial-{n}", factorial, n)
-    assert p.result() == 2  # noqa: PLR2004
+    n = 5
+    p: Promise[int] = s.run(f"factorial-same-node-{n}", factorial, n)
+    assert p.result() == 120  # noqa: PLR2004
+
+
+@pytest.mark.skipif(
+    os.getenv("RESONATE_STORE_URL") is None, reason="env variable is not set"
+)
+def test_factorial_multi_node() -> None:
+    def factorial_node_1(ctx: Context, n: int) -> Generator[Yieldable, Any, int]:
+        if n == 0:
+            return 1
+        return n * (
+            yield ctx.rfc(factorial_node_1, n - 1).with_options(
+                f"factorial-multi-node-{n-1}", target="test-factorial-multi-node-2"
+            )
+        )
+
+    def factorial_node_2(ctx: Context, n: int) -> Generator[Yieldable, Any, int]:
+        if n == 0:
+            return 1
+        return n * (
+            yield ctx.rfc(factorial_node_2, n - 1).with_options(
+                f"factorial-multi-node-{n-1}", target="test-factorial-multi-node-1"
+            )
+        )
+
+    store = RemoteServer(url=os.environ["RESONATE_STORE_URL"])
+    s1 = Scheduler(store, logic_group="test-factorial-multi-node-1")
+    s1.register(factorial_node_1, name="factorial")
+    s2 = Scheduler(store, logic_group="test-factorial-multi-node-2")
+    s2.register(factorial_node_2, name="factorial")
+    n = 5
+    p: Promise[int] = s1.run(f"factorial-multi-node-{n}", factorial_node_1, n)
+    assert p.result() == 120  # noqa: PLR2004
