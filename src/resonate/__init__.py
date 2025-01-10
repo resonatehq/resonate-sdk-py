d3cccf0f1f4d4cf132ea346fe729fad5022b429f
Tomperez98 <tomasperezalvarez@gmail.com>
2025-01-10 12:06:42 -0500

    Allow to import ctx from top lvl

--- src/resonate/__init__.py
+++ src/resonate/__init__.py
@@ -1,13 +1,9 @@
 from __future__ import annotations
 
+from resonate.context import Context
 from resonate.dataclasses import DurablePromise
 from resonate.handle import Handle
 from resonate.promise import Promise
 from resonate.resonate import Resonate
 
-__all__ = [
-    "Resonate",
-    "Handle",
-    "DurablePromise",
-    "Promise",
-]
+__all__ = ["Resonate", "Handle", "DurablePromise", "Promise", "Context"]
