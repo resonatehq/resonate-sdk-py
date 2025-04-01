01d31b3fbff33f19700bad9f074c0bca76940575
Tomperez98 <tomasperezalvarez@gmail.com>
2025-04-01 16:45:30 -0500

    tests passing

--- tests/test_retries.py
+++ tests/test_retries.py
@@ -6,29 +6,34 @@ import pytest
 
 from resonate.dependencies import Dependencies
 from resonate.models.commands import Delayed, Function, Invoke, Network, RejectPromiseReq, ResolvePromiseReq, Return
-from resonate.models.durable_promise import DurablePromise
+from resonate.models.durable_promise import DurablePromise  # noqa: TC001
 from resonate.models.options import Options
 from resonate.models.result import Ko, Ok
 from resonate.models.retry_policies import Constant, Exponential, Linear, Never, RetryPolicy
-from resonate.models.task import Task
+from resonate.models.task import Task  # noqa: TC001
 from resonate.registry import Registry
 from resonate.resonate import Context
 from resonate.scheduler import Scheduler
 from resonate.stores.local import LocalStore
 
 
-def foo(ctx: Context):
+def foo(ctx: Context):  # noqa: ANN201
     return "foo"
 
 
-def bar(ctx: Context):
+def bar_ok(ctx: Context):  # noqa: ANN201
+    return "bar"
+    yield ctx.lfi(foo)
+
+
+def bar_ko(ctx: Context):  # noqa: ANN201
+    raise ValueError
     yield ctx.lfi(foo)
 
 
 @pytest.fixture
 def scheduler() -> Scheduler:
-    s = Scheduler(lambda id, info: Context(id, info, Options(), Registry(), Dependencies()))
-    return s
+    return Scheduler(lambda id, info: Context(id, info, Options(), Registry(), Dependencies()))
 
 
 @pytest.fixture
@@ -49,11 +54,11 @@ def promise_and_task() -> tuple[DurablePromise, Task]:
     ],
 )
 def test_function_happy_path(scheduler: Scheduler, promise_and_task: tuple[DurablePromise, Task], retry_policy: RetryPolicy) -> None:
-    reqs = scheduler._step(Invoke("foo", "foo", foo, opts=Options(retry_policy=retry_policy), promise_and_task=promise_and_task))
+    reqs = scheduler._step(Invoke("foo", "foo", foo, opts=Options(retry_policy=retry_policy), promise_and_task=promise_and_task))  # noqa: SLF001
     assert len(reqs) == 1
     req = reqs[0]
     assert isinstance(req, Function)
-    reqs = scheduler._step(Return("foo", "foo", Ok("foo")))
+    reqs = scheduler._step(Return("foo", "foo", Ok("foo")))  # noqa: SLF001
     assert len(reqs) == 1
     req = reqs[0]
     assert isinstance(req, Network)
@@ -70,18 +75,18 @@ def test_function_happy_path(scheduler: Scheduler, promise_and_task: tuple[Durab
     ],
 )
 def test_function_sad_path(scheduler: Scheduler, promise_and_task: tuple[DurablePromise, Task], retry_policy: RetryPolicy, retries: int) -> None:
-    reqs = scheduler._step(Invoke("foo", "foo", foo, opts=Options(retry_policy=retry_policy), promise_and_task=promise_and_task))
+    reqs = scheduler._step(Invoke("foo", "foo", foo, opts=Options(retry_policy=retry_policy), promise_and_task=promise_and_task))  # noqa: SLF001
     assert len(reqs) == 1
     req = reqs[0]
     assert isinstance(req, Function)
 
     for _ in range(retries):
-        reqs = scheduler._step(Return("foo", "foo", Ko(ValueError("something went wrong"))))
+        reqs = scheduler._step(Return("foo", "foo", Ko(ValueError("something went wrong"))))  # noqa: SLF001
         assert len(reqs) == 1
         req = reqs[0]
         assert isinstance(req, Delayed)
 
-    reqs = scheduler._step(Return("foo", "foo", Ko(ValueError("something went wrong"))))
+    reqs = scheduler._step(Return("foo", "foo", Ko(ValueError("something went wrong"))))  # noqa: SLF001
     assert len(reqs) == 1
     req = reqs[0]
     assert isinstance(req, Network)
@@ -98,13 +103,31 @@ def test_function_sad_path(scheduler: Scheduler, promise_and_task: tuple[Durable
     ],
 )
 def test_generator_happy_path(scheduler: Scheduler, promise_and_task: tuple[DurablePromise, Task], retry_policy: RetryPolicy) -> None:
-    reqs = scheduler._step(Invoke("foo", "foo", bar, opts=Options(retry_policy=retry_policy), promise_and_task=promise_and_task))
-    assert len(reqs) == 1
-    req = reqs[0]
-    print(req)
-    assert isinstance(req, Function)
-    reqs = scheduler._step(Return("bar", "bar", Ok("bar")))
+    reqs = scheduler._step(Invoke("foo", "foo", bar_ok, opts=Options(retry_policy=retry_policy), promise_and_task=promise_and_task))  # noqa: SLF001
     assert len(reqs) == 1
     req = reqs[0]
     assert isinstance(req, Network)
     assert isinstance(req.req, ResolvePromiseReq)
+
+
+@pytest.mark.parametrize(
+    ("retry_policy", "retries"),
+    [
+        (Never(), 0),
+        (Constant(max_retries=2), 2),
+        (Linear(max_retries=3), 3),
+        (Exponential(max_retries=1), 1),
+    ],
+)
+def test_generator_sad_path(scheduler: Scheduler, promise_and_task: tuple[DurablePromise, Task], retry_policy: RetryPolicy, retries: int) -> None:
+    reqs = scheduler._step(Invoke("foo", "foo", bar_ko, opts=Options(retry_policy=retry_policy), promise_and_task=promise_and_task))  # noqa: SLF001
+    assert len(reqs) == 1
+    req = reqs[0]
+    for _ in range(retries):
+        assert isinstance(req, Delayed)
+        reqs = scheduler._step(req.item)  # noqa: SLF001
+        assert len(reqs) == 1
+        req = reqs[0]
+
+    assert isinstance(req, Network)
+    assert isinstance(req.req, RejectPromiseReq)
