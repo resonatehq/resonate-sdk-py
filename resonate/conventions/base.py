be42650e6f6564b8aefbd6f1832d71fd601ec772
Tomperez98 <tomasperezalvarez@gmail.com>
2025-04-23 15:18:04 -0500

    Enable HITL with Base convention

---
+++ resonate/conventions/base.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from typing import Any
+
+from resonate.errors.errors import ResonateValidationError
+from resonate.options import Options
+
+
+@dataclass
+class Base:
+    data: Any
+    headers: dict[str, str] | None
+    opts: Options = field(default_factory=Options)
+
+    def format(self) -> tuple[Any, dict[str, str], int, dict[str, str] | None]:
+        return self.data, self.opts.tags, self.opts.timeout, self.headers
+
+    def options(self, send_to: str | None, tags: dict[str, str] | None, timeout: int | None, version: int | None) -> None:
+        if send_to is not None or version is not None:
+            msg = "`send_to` and `version` cannot be set for the convention."
+            raise ResonateValidationError(msg)
+        self.opts.merge(timeout=timeout, tags=tags)
