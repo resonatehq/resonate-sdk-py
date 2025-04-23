16d509e2df7026effdac5915addb8054ef16422b
Tomperez98 <tomasperezalvarez@gmail.com>
2025-04-23 17:44:00 -0500

    rollback ranme

--- resonate/models/handle.py
+++ resonate/models/handle.py
@@ -10,7 +10,7 @@ class Handle[T]:
     def __init__(self, f: Future[T]) -> None:
         self._f = f
 
-    def is_resolved(self) -> bool:
+    def done(self) -> bool:
         return self._f.done()
 
     def result(self) -> T:
