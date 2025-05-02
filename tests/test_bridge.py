d301027fedb855a591e00b54cbc15ddd30d760f4
Tomperez98 <tomasperezalvarez@gmail.com>
2025-05-01 19:12:04 -0500

    tests passing

--- tests/test_bridge.py
+++ tests/test_bridge.py
@@ -230,9 +230,9 @@ def test_sleep(resonate_instance: Resonate) -> None:
 
 def test_handle_timeout(resonate_instance: Resonate) -> None:
     timestamp = int(time.time())
-    handle = resonate_instance.run(f"sleep-{timestamp}", sleep, 1)
+    handle = resonate_instance.run(f"handle-timeout-{timestamp}", sleep, 1)
     with pytest.raises(TimeoutError):
-        handle.result(timeout=0)
+        handle.result(timeout=0.1)
     assert handle.result() == 1
 
 
