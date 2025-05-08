fae1a32e94cbea070455d566488dbee50f981211
Tomperez98 <tomasperezalvarez@gmail.com>
2025-05-08 13:57:51 -0500

    fix typo

--- tests/test_bridge.py
+++ tests/test_bridge.py
@@ -152,7 +152,7 @@ def parent_bound(ctx: Context, child_timeout_rel: int) -> Generator[Yieldable, A
 
 
 def child_bounded(ctx: Context, parent_timeout_abs: float) -> None:
-    assert not (ctx.info.timeout > parent_timeout_abs) # child timeout never exceed parent timeout
+    assert not (ctx.info.timeout > parent_timeout_abs) # child timeout never exceeds parent timeout
 
 
 @pytest.fixture(scope="module")
