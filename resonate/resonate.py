68472b81243e63660e9689a8c1799c15fbf80945
Tomás Pérez Álvarez <72174660+Tomperez98@users.noreply.github.com>
2025-06-25 11:42:11 -0500

    Update resonate/resonate.py
    Co-authored-by: David Farr <david@resonatehq.io>

--- resonate/resonate.py
+++ resonate/resonate.py
@@ -649,7 +649,7 @@ class Context:
         result.
 
         - Function must be registered.
-        - Function *args and **kwargs must be encodable.
+        - Function *args and **kwargs must be serializable.
         """
         name, _, version = (func, None, self._registry.latest(func)) if isinstance(func, str) else self._registry.get(func)
         return RFI(Remote(self._next(), self._cid, self._id, name, args, kwargs, Options(version=version)))
