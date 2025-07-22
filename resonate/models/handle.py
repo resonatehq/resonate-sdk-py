068071559fd5111dc1ea2d5172c3dea5234712da
Tomperez98 <tomasperezalvarez@gmail.com>
2025-07-22 08:33:26 -0500

    Expose future

--- resonate/models/handle.py
+++ resonate/models/handle.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from collections.abc import Callable
+from types import GenericAlias
+from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
     from concurrent.futures import Future
@@ -8,10 +10,10 @@ if TYPE_CHECKING:
 
 class Handle[T]:
     def __init__(self, f: Future[T]) -> None:
-        self._f = f
+        self.future = f
 
     def done(self) -> bool:
-        return self._f.done()
+        return self.future.done()
 
     def result(self, timeout: float | None = None) -> T:
-        return self._f.result(timeout)
+        return self.future.result(timeout)
