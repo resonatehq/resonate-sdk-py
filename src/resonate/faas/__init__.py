56d51e72ffd5da564bfe943e0e84c34b61242972
Tomperez98 <tomasperezalvarez@gmail.com>
2026-07-11 13:06:23 -0500

    faas aws

---
+++ src/resonate/faas/__init__.py
@@ -0,0 +1,5 @@
+"""Serverless (Function-as-a-Service) entrypoints for the Resonate SDK.
+
+Each submodule adapts :class:`resonate.core.Core` to a specific serverless
+platform. See :mod:`resonate.faas.aws` for AWS Lambda.
+"""
